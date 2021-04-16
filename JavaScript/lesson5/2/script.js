01d5ec71fb35c85785dabaa48c82395665f869af
IgorRomanovv <73357160+IgorRomanovv@users.noreply.github.com>
2021-04-16 13:28:54 +0300

    Исправдения ошибок

--- JavaScript/lesson5/2/script.js
+++ JavaScript/lesson5/2/script.js
@@ -1,6 +1,6 @@
 "use strict"
 const n = +prompt('Введите целое положительное число');
-for (let i = 0; i <= n * 2; i = i + 2) {
+for (let i = 0; i <= n * 2; i += 2) {
     if (i % 5 != 0) {
         alert ( i )
     };
